b49b4009a2e3874c576aa56164b6e9bd6016aeb9
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-02-05 12:23:32 +0900

    set-up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        nanum: ['NanumSquare', 'sans-serif'], //globals.css에서 나눔스퀘어 임포트, 폴백으로 산셰리프 고딕체 추가하였습니다.
       },
     },
   },
