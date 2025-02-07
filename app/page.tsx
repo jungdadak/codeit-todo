e7629d2702ccc64b37fb816726b1a9563edf9e75
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-02-07 16:21:51 +0900

    skeleton ui upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ function TodoListSkeleton() {
   return (
     <div className="animate-pulse">
       <div className="w-full m-3 p-3 h-14 bg-slate-200 rounded-full"></div>
-      <div className="w-full m-3 p-3 h-14 bg-slate-200 rounded-full"></div>
+      <div className="w-full m-3 p-3 h-14 bg-slate-200 rounded-full text-16 text-violet-400 flex items-center justify-center gap-3">
+        <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900" />
+        최신 데이터 불러오는중 ...
+      </div>
       <div className="w-full m-3 p-3 h-14 bg-slate-200 rounded-full"></div>
     </div>
   );
@@ -15,7 +18,7 @@ function TodoListSkeleton() {
 
 export default function Home() {
   return (
-    <div className="text-slate-900">
+    <div className="text-slate-900 min-h-screen-minus-nav">
       <SearchBox />
       <div className="flex flex-col lg:flex-row justify-center gap-5 mt-10">
         <section className="text-center w-full lg:w-1/2">
