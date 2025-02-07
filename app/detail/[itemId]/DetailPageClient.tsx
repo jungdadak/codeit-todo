3d883122351b7b7b0e9825ca40f1b1f399292982
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-02-07 16:14:19 +0900

    tanstack cache disabled, detailpage 네임카드컴포넌트화함

--- app/detail/[itemId]/DetailPageClient.tsx
+++ app/detail/[itemId]/DetailPageClient.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import Image from 'next/image';
 import { useToast } from '@/hooks/use-toast';
 import ImageUpload from './ImageUpload';
 import MemoInput from './MemoInput';
