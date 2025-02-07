a98678700eb0e3a3105f32a9f1957379022a5e9c
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-02-07 16:12:23 +0900

    tanstack cache delete

--- app/TanstackProvider.tsx
+++ app/TanstackProvider.tsx
@@ -3,6 +3,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 
+/**
+ * 이미 삭제된 항목이 캐시에 남아 있어
+ * 모든 필드에서 캐시를 사용하지 않고 최신 데이터를 사용하기 위해
+ * 옵션 설정을 추가했습니다.
+ */
 export default function TanstackProviders({
   children,
 }: {
@@ -13,7 +18,11 @@ export default function TanstackProviders({
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 0, // 캐시 무효화하여 최신 데이터만 표시
+            staleTime: 0, // 항상 최신 데이터를 요청
+            gcTime: 0, // 가비지 컬렉션 타임 (이전의 cacheTime)
+            refetchOnWindowFocus: true, // 윈도우 포커스시 새로고침
+            retry: false, // 실패시 재시도 하지 않음
+            refetchOnMount: true, // 컴포넌트 마운트시 새로고침
           },
         },
       })
